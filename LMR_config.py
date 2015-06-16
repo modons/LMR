4d6e01db20a34c10d23710588ae4861ade0058dd
Andre Perkins <frodre@gmail.com>
2015-06-16 16:00:40 -0700

    LMR_config cleanup
    Added docstrings to each config section describing attributes.  Removed
    extraneous comments.  Added archive directory attribute.

--- LMR_config.py
+++ LMR_config.py
@@ -6,109 +6,150 @@ Adapted from LMR_exp_NAMELIST by AndreP
 """
 
 
-# =============================================================================
-# Section 1: High-level parameters of reconstruction experiment
-# =============================================================================
-
 class core:
     """
     High-level parameters of reconstruction experiment
+
+    Attributes
+    ----------
+    nexp: str
+        Name of reconstruction experiment
+    lmr_path: str
+        Absolute path for the experiment
+    clean_start: bool
+        Delete existing files in output directory (otherwise they will be used
+        as the prior!)
+    recon_period: list(int)
+        Time period for reconstruction
+    nens: int
+        Ensemble size
+    proxy_frac: float
+        Fraction of available proxy data (sites) to assimilate
+    iter_range: list(int)
+        Number of Monte-Carlo iterations to perform
+    loc_rad: float
+        Localization radius for DA (in km)
+    datadir_output: str
+        Absolute path to working directory output for LMR
+    archive_dir: str
+        Absolute path to LMR reconstruction archive directory
     """
-    # Name of reconstruction experiment
     nexp = 'testdev_1000_75pct'
-
-    # set the absolute path the experiment
-    LMRpath = '/home/chaos2/wperkins/data/LMR'
-
-    # set clean_start to True to delete existing files in the outpout directory
-    # (otherwise they will be used as the prior!)
+    lmr_path = '/home/chaos2/wperkins/data/LMR'
     clean_start = True
-
-    # Reconstruction period (years)
     recon_period = [1000, 2000]
-
-    # Ensemble size
-    Nens = 100
-
-    # Fraction of available proxy data (sites) to assimilate
-    # (=1.0 for all, 0.5 for half etc.)
+    nens = 100
     proxy_frac = 0.75
+    # TODO: Monte-Carlo section, also can replace with single int
+    iter_range = [0, 10]
+    loc_rad = None
 
-    # Number of Monte-Carlo iterations
-    iter_range = [0,10]
+    datadir_output = '/home/chaos2/wperkins/data/LMR/output/working'
+    #datadir_output  = '/home/disk/kalman3/rtardif/LMR/output/wrk'
+    #datadir_output  = '/home/disk/ekman/rtardif/nobackup/LMR/output'
+    #datadir_output  = '/home/disk/ice4/hakim/svnwork/python-lib/trunk/src/ipython_notebooks/data'
 
-    # Localization radius for DA (in km)
-    locRad = None
-    #locRad = 2000.0
-    #locRad = 10000.0
-
-# =============================================================================
-# Section 2: PROXIES
-# =============================================================================
+    archive_dir = '/home/chaos2/wperkins/data/LMR/output/archive'
+    #archive_dir = '/home/disk/kalman3/rtardif/LMR/output'
+    #archive_dir = '/home/disk/kalman3/hakim/LMR/'
 
 class proxies:
-    # Proxy data directory & file
-    datadir_proxy    = core.LMRpath+'/proxies'
+    """
+    Parameters for proxy data
+
+    Attributes
+    ----------
+    datadir_proxy: str
+        Absolute path to proxy data
+    datafile_proxy: str
+        Proxy data file name
+    dataformat_proxy: str
+        File format of the proxy data
+    regions: list(str)
+        List of proxy data regions (data keys) to use.
+    proxy_resolution: list(float)
+        List of proxy time resolutions to use
+    proxy_assim: dict{ str: list(str)}
+        Proxy types to be assimilated.
+        Uses dictionary with structure {<<proxy type>>: [.. list of measuremant
+        tags ..] where "proxy type" is written as
+        "<<archive type>>_<<measurement type>>"
+    dat_filters:
+    datatag_calib: str
+        Source of calibration data for PSM
+    datadir_calib: str
+        Absolute path to calibration data
+    datafile_calib: str
+        Filename for calibration data
+    dataformat_calib: str
+        Data storage type for calibration data
+    psm_r_crit: float
+        Threshold correlation of linear PSM
+    """
+    #TODO: Most of this is very specific to proxies from Pages, not general
+    datadir_proxy    = core.lmr_path+'/proxies'
     datafile_proxy   = 'Pages2k_db_metadata.df'
     dataformat_proxy = 'DF'
-
     regions = ['Antarctica', 'Arctic', 'Asia', 'Australasia', 'Europe',
                'North America', 'South America']
-
-    # Proxy temporal resolution (in yrs)
-    # proxy_resolution = [1.0,5.0]
     proxy_resolution = [1.0]
 
-    # Define proxies to be assimilated
-    # Use dictionary with structure: {<<proxy type>>:[... list of all measurement tags ...]}
-    # where "proxy type" is written as "<<archive type>>_<<measurement type>>"
     # DO NOT CHANGE FORMAT BELOW
     proxy_assim = {
-        '01:Tree ring_Width': ['Ring width','Tree ring width','Total ring width','TRW'],
-        '02:Tree ring_Density': ['Maximum density','Minimum density','Earlywood density','Latewood density','MXD'],
+        '01:Tree ring_Width': ['Ring width',
+                               'Tree ring width',
+                               'Total ring width',
+                               'TRW'],
+        '02:Tree ring_Density': ['Maximum density',
+                                 'Minimum density',
+                                 'Earlywood density',
+                                 'Latewood density',
+                                 'MXD'],
         '03:Ice core_d18O': ['d18O'],
         '04:Ice core_d2H': ['d2H'],
-        '05:Ice core_Accumulation':['Accumulation'],
+        '05:Ice core_Accumulation': ['Accumulation'],
         '06:Coral_d18O': ['d18O'],
-        '07:Coral_Luminescence':['Luminescence'],
-        '08:Lake sediment_All':['Varve thickness','Thickness','Mass accumulation rate','Particle-size distribution','Organic matter','X-ray density'],
-        '09:Marine sediment_All':['Mg/Ca'],
-        '10:Speleothem_All':['Lamina thickness'],
+        '07:Coral_Luminescence': ['Luminescence'],
+        '08:Lake sediment_All': ['Varve thickness',
+                                 'Thickness',
+                                 'Mass accumulation rate',
+                                 'Particle-size distribution',
+                                 'Organic matter',
+                                 'X-ray density'],
+        '09:Marine sediment_All': ['Mg/Ca'],
+        '10:Speleothem_All': ['Lamina thickness'],
         }
-
     dat_filters = {'PAGES 2k Region': regions,
                    'Resolution (yr)': proxy_resolution,
                    'Proxy measurement': proxy_assim}
-
-    # Source of calibration data (for PSM)
     datatag_calib = 'GISTEMP'
-    #datatag_calib = 'HadCRUT'
-    #datatag_calib = 'BerkeleyEarth'
-    datadir_calib = core.LMRpath+'/analyses'
-
-    # Threshold correlation of linear PSM
-    PSM_r_crit = 0.2
+    datadir_calib = core.lmr_path+'/analyses'
+    datafile_calib = 'gistemp1200_ERSST.nc'
+    dataformat_calib = 'NCD'
+    psm_r_crit = 0.2
 
 # =============================================================================
 # Section 3: MODEL (PRIOR)
 # =============================================================================
 
 class prior:
+    """
+    Parameters for the ensDA prior
+
+    prior_source: str
+        Source of prior data
+    datadir_prior: str
+        Absolute path to prior data
+    datafile_prior: str
+        Name of prior file to use
+    dataformat_prior: str
+        Datatype of prior container
+    state_variables: list(str)
+        List of variables to use in the state vector for the prior
+    """
     # Prior data directory & model source
     prior_source = 'ccsm4_last_millenium'
     datadir_prior = '/home/chaos2/wperkins/data/'
     datafile_prior = 'tas_Amon_CCSM4_past1000_r1i1p1_085001-185012.nc'
     dataformat_prior = 'NCD'
-
-    # Define variables in state vector (will be updated by assimilation)
     state_variables = ['Tsfc']
-    #state_variables = ['Tsfc', 'h500']
-
-# =============================================================================
-# Section 4: OUTPUT
-# =============================================================================
-
-# Output
-#datadir_output  = '/home/disk/kalman3/rtardif/LMR/output'
-#datadir_output  = '/home/disk/ekman/rtardif/nobackup/LMR/output'
-datadir_output = '/home/chaos2/wperkins/data/LMR/output'
