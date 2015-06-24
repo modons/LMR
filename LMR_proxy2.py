d6f2f22efb1d9b8048d433c9db8e7bfae4fc3ccb
Andre Perkins <frodre@gmail.com>
2015-06-24 09:26:50 -0700

    Abstract class ideas for the module.

--- LMR_proxy2.py
+++ LMR_proxy2.py
@@ -1,1 +1,30 @@
 __author__ = 'frodre'
+
+"""
+Module containing proxy classes
+"""
+
+from abc import ABCMeta, abstractmethod
+
+class BaseProxyManager:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def create_proxies(self):
+        pass
+
+
+class BaseProxyObject:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def read_proxy(self):
+        pass
+
+    @abstractmethod
+    def psm(self):
+        pass
+
+    @abstractmethod
+    def error(self):
+        pass
--- load_data.py
+++ load_data.py
@@ -1,1 +1,11 @@
 __author__ = 'frodre'
+
+"""
+General data loading functions
+"""
+
+def query_dataframe(filters):
+    pass
+
+def grab_record_dataframe(columns):
+    pass
