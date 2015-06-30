f4ae91eb042310ff5f7d45784ee3a6dc301376e8
Andre Perkins <frodre@gmail.com>
2015-06-30 15:19:49 -0600

    Added simple method to check datasource and load a dataframe if necessary

--- load_data.py
+++ load_data.py
@@ -4,8 +4,16 @@ __author__ = 'frodre'
 General data loading functions
 """
 
+import pandas
+
 def query_dataframe(filters):
     pass
 
 def grab_record_dataframe(columns):
     pass
+	
+def load_data_frame(data_src):
+	if type(data_src) == pandas.DataFrame:
+		return data_src
+	else:
+		pandas.read_pickle(data_src)
