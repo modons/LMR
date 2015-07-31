8fa4e06e6966e64339461c7b843964d81d0cb183
Andre Perkins <frodre@gmail.com>
2015-07-31 10:05:37 -0700

    Update to structure, is now one class.  Still need to figure out averaging.

--- LMR_gridded.py
+++ LMR_gridded.py
@@ -24,66 +24,46 @@ _ALT_DIMENSION_DEFS = {'latitude': _LAT,
                        'plev': _LEV}
 
 
-class GriddedData:
-    __metaclass__ = ABCMeta
+class GriddedVariable(object):
 
-    def __init__(self, name, vartype, dim_order, data, time,
+    def __init__(self, name, dims_ordered, data, time=None,
                  lev=None, lat=None, lon=None):
         self.name = name
-        self.vartype = vartype
-        self.dim_order = dim_order
+        self.dim_order = dims_ordered
+        self.ndim = len(dims_ordered)
         self.data = data
         self.time = time
         self.lev = lev
         self.lat = lat
         self.lon = lon
 
-    @classmethod
-    @abstractmethod
-    def _load_from_netcdf(cls):
-        pass
+        space_dims = [dim for dim in dims_ordered if dim != _TIME]
 
-    @classmethod
-    def load(cls, config, file_type):
-        if file_type == constants.file_types.netcdf:
-            cls._load_from_netcdf(config)
+        if len(space_dims) > 2:
+            raise ValueError('Class cannot handle 3D data yet!')
+
+        if not space_dims:
+            self.type = 'timeseries'
+        elif _LAT in space_dims and _LON in space_dims:
+            self.type = 'horizontal'
+        elif _LAT in space_dims and _LEV in space_dims:
+            self.type = 'meridional_vertical'
         else:
-            raise TypeError('Specified file type not supported yet.')
-
-    @staticmethod
-    def netcdf_datetime_convert(time_var):
-        try:
-            time = num2date(time_var[:], units=time_var.units,
-                                calendar=time_var.calendar)
-            return  time.tolist()
-        except ValueError:
-            # num2date needs calendar year start >= 0001 C.E. (bug submitted
-            # to unidata about this
-            fmt = '%Y-%d-%m %H:%M:%S'
-            tunits = time_var.units
-            since_yr_idx = tunits.index('since ') + 6
-            year = int(tunits[since_yr_idx:since_yr_idx+4])
-            year_diff = year - 0001
-            new_start_date = datetime(0001, 01, 01, 0, 0, 0)
-
-            new_units = tunits[:since_yr_idx] + '0001-01-01 00:00:00'
-            time_yrs = num2date(time_var[:], new_units, calendar=time_var.calendar)
-            return [datetime(d.year + year_diff, d.month, d.day,
-                             d.hour, d.minute, d.second)
-                    for d in time_yrs]
-
-
-class PriorVariable(GriddedData):
+            raise ValueError('Unrecognized dimension combination.')
 
     @classmethod
     def _load_from_netcdf(cls, config, varname):
 
-        dir = config.prior.datadir_prior
-        file = config.prior.datafile_prior
+        dir_name = config.prior.datadir_prior
+        filename = config.prior.datafile_prior
+        pre_avg_name = '.pre_avg'
 
+        pre_avg_exists = False
         # Check for pre-averaged
+        if os.path.exists(dir_name+filename+pre_avg_name):
+            pre_avg_exists = True
 
-        with Dataset(dir+file, 'r') as f:
+        with Dataset(dir_name+filename, 'r') as f:
             var = f.variables[varname]
             data_shp = var.shape
 
@@ -116,12 +96,36 @@ class PriorVariable(GriddedData):
             # Convert time to years
             dim_vals[_TIME] = cls.netcdf_datetime_convert(dim_vals[_TIME])
 
-            # Determine Prior TYPE
+            # Extract data for each dimension
+            dim_vales = {k: val[:] for k, val in dim_vals.iteritems()}
 
-            return cls(varname, )
+            return cls(varname, dims, var[:], **dim_vals)
 
+    @classmethod
+    def load(cls, config, file_type):
+        if file_type == constants.file_types.netcdf:
+            cls._load_from_netcdf(config)
+        else:
+            raise TypeError('Specified file type not supported yet.')
 
+    @staticmethod
+    def netcdf_datetime_convert(time_var):
+        try:
+            time = num2date(time_var[:], units=time_var.units,
+                                calendar=time_var.calendar)
+            return  time.tolist()
+        except ValueError:
+            # num2date needs calendar year start >= 0001 C.E. (bug submitted
+            # to unidata about this
+            fmt = '%Y-%d-%m %H:%M:%S'
+            tunits = time_var.units
+            since_yr_idx = tunits.index('since ') + 6
+            year = int(tunits[since_yr_idx:since_yr_idx+4])
+            year_diff = year - 0001
+            new_start_date = datetime(0001, 01, 01, 0, 0, 0)
 
-
-
-def _unload_netcdf_dims(dat_shp, ncf_dimensions):
+            new_units = tunits[:since_yr_idx] + '0001-01-01 00:00:00'
+            time_yrs = num2date(time_var[:], new_units, calendar=time_var.calendar)
+            return [datetime(d.year + year_diff, d.month, d.day,
+                             d.hour, d.minute, d.second)
+                    for d in time_yrs]
