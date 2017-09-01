fixed biased numpy variance calls with ddof=1 in DA routine; utils; proxy codes

LMR_DA.py
@@ -17,6 +17,14 @@ def enkf_update_array(Xb, obvalue, Ye, ob_err, loc=None, inflate=None):
 
     Originator: G. J. Hakim, with code borrowed from L. Madaus
                 Dept. Atmos. Sciences, Univ. of Washington
+
+    Revisions:
+
+    1 September 2017: 
+                    - changed varye = np.var(Ye) to varye = np.var(Ye,ddof=1) 
+                    for an unbiased calculation of the variance. 
+                    (G. Hakim - U. Washington)
+    
     -----------------------------------------------------------------
      Inputs:
           Xb: background ensemble estimates of state (Nx x Nens) 
@@ -36,7 +44,7 @@ def enkf_update_array(Xb, obvalue, Ye, ob_err, loc=None, inflate=None):
 
     # ensemble mean and variance of the background estimate of the proxy 
     mye   = np.mean(Ye)
-    varye = np.var(Ye)
+    varye = np.var(Ye,ddof=1)
 
     # lowercase ye has ensemble-mean removed 
     ye = np.subtract(Ye, mye)

LMR_proxy.py
@@ -184,7 +184,7 @@ class proxy_master(object):
 
             if diag_output:
                 # Some other diagnostics
-                varproxy = np.var(reg_ya); varresiduals = np.var(residuals)
+                varproxy = np.var(reg_ya,ddof=1); varresiduals = np.var(residuals,ddof=1)
                 varratio = np.divide(varproxy,varresiduals)
                 snr = np.absolute(r_value)/(np.sqrt(1.0-r_value**2))            
                 # Diagnostic output 

LMR_utils.py
@@ -458,7 +458,7 @@ def ensemble_stats(workdir, y_assim, write_posterior_Ye=False, save_full_field=F
             # 0D:time series variable (no spatial dims)
             Xb = Xbtmp[ibeg:iend+1,:] # prior (full) ensemble
             xbm = np.mean(Xb,axis=1)  # ensemble mean
-            xbv = np.var(Xb,axis=1)   # ensemble variance
+            xbv = np.var(Xb,axis=1,ddof=1)   # ensemble variance
 
             # process the **analysis** files
             years = []
@@ -477,7 +477,7 @@ def ensemble_stats(workdir, y_assim, write_posterior_Ye=False, save_full_field=F
                 Xa = Xatmp[ibeg:iend+1,:]
                 xa_ens[k] = Xa  # total ensemble
                 xam[k] = np.mean(Xa,axis=1) # ensemble mean
-                xav[k] = np.var(Xa,axis=1)  # ensemble variance
+                xav[k] = np.var(Xa,axis=1,ddof=1)  # ensemble variance
 
             vars_to_save_ens = {'nens':nens, 'years':years, 'xb_ens':Xb, 'xa_ens':xa_ens}
             vars_to_save_mean = {'nens':nens, 'years':years, 'xbm':xbm, 'xam':xam}