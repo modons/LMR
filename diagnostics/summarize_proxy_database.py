2cec2b591878b38e3a4b4113caafd1a3fa52c683
Robert Tardif <rtardif@atmos.washington.edu>
2017-11-16 12:12:23 -0800

    Minor adjustments to plotting in diagnostics and more info. provided to screen in build_ye_file.py

--- diagnostics/summarize_proxy_database.py
+++ diagnostics/summarize_proxy_database.py
@@ -203,10 +203,13 @@ for ptype in sorted(proxy_types):
         #ax  = fig.add_axes([0.1,0.1,0.8,0.8])
         m = Basemap(projection='robin', lat_0=0, lon_0=0,resolution='l', area_thresh=700.0); latres = 20.; lonres=40.  # GLOBAL
 
-        water = '#9DD4F0'
-        continents = '#888888'
+        #water = '#9DD4F0'
+        #continents = '#888888'
+        water = '#D3ECF8'
+        continents = '#F2F2F2'
+        
         m.drawmapboundary(fill_color=water)
-        m.drawcoastlines(); m.drawcountries()
+        m.drawcoastlines((linewidth=0.5); m.drawcountries()
         m.fillcontinents(color=continents,lake_color=water)
         m.drawparallels(np.arange(-80.,81.,latres))
         m.drawmeridians(np.arange(-180.,181.,lonres))
